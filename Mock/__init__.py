ea777f78e8a93d4bd20264bdb541ad74c2b1083b
Tiago Costa <tiagorc@gmail.com>
2019-12-26 08:06:43 +0000

    restore fn debug info

--- Mock/__init__.py
+++ Mock/__init__.py
@@ -1,8 +1,6 @@
-import logging
-import json
-import os
-from io import BytesIO
-from zipfile import ZipFile, ZIP_DEFLATED as deflated
+from logging import info as log
+from json import dumps as stringify
+from os import getenv
 from datetime import datetime, timezone
 import azure.functions as func
 from azure.core.exceptions import ResourceExistsError
@@ -10,11 +8,11 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
-    logging.info('Generating request dump')
+    log('Generating request dump')
 
-    connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    connection_string = getenv("AZURE_STORAGE_CONNECTION_STRING")
     container_name = "uploads"
-    file_name = datetime.now(timezone.utc).isoformat() + ".zip"
+    file_name = datetime.now(timezone.utc).isoformat()
 
     request = {
        "url": req.url,
@@ -25,19 +23,30 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if hasattr(req, "form") and len(req.form) > 0:
         request["form"] = req.form
 
-    mem_zip = BytesIO()
+    data = None
 
-    with ZipFile(mem_zip, mode="w", compression=deflated) as zf:
-        zf.writestr("request.txt", json.dumps(request))
-        if hasattr(req, "files") and len(req.files) > 0:
-            logging.info('Attaching request files')
+    if hasattr(req, "files") and len(req.files) > 0:
+
+        from io import BytesIO
+        from zipfile import ZipFile, ZIP_DEFLATED as deflated
+
+        log('Creating zip file')
+        mem_zip = BytesIO()
+
+        with ZipFile(mem_zip, mode="w", compression=deflated) as zf:
+            zf.writestr("request.txt", stringify(request))
             for file in req.files:
                 file_data = req.files[file].stream.read()
                 zf.writestr(file, file_data)
 
-    data = mem_zip.getvalue()
+        data = mem_zip.getvalue()
+        file_name += ".zip"
 
-    logging.info('Accessing blob container')
+    else:
+        data = stringify(request).encode('utf-8')
+        file_name += ".txt"
+
+    log('Accessing blob container')
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     container_client = blob_service_client.get_container_client(container_name)
 
@@ -46,7 +55,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except ResourceExistsError:        
         pass
     
-    logging.info('Writing to blob storage')
+    log('Writing to blob storage')
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
     blob_client.upload_blob(data)
 
